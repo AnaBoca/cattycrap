846ab2ef300c686db890a051a8962e3d810def12
Ana Boca <Ana.Boca@asu.edu>
2019-12-23 00:18:43 +0000

    more validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :user
-  validates :vaccine_type, presence: true
+  validates :vaccine_type, :date, :notes, length: { minimum: 2, maximum: 50 }, presence: true
 end
