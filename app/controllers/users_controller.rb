01e715a45be28fa196b630e2be057aa8ef28f618
Ana Boca <Ana.Boca@asu.edu>
2020-01-06 01:50:32 +0000

    vaccine content on index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @events = Event.paginate(page: params[:page], per_page: 10)
+    @events = @user.events.order('created_at DESC').paginate(page: params[:page], per_page: 10)
   end
 
 end
